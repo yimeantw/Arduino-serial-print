28f4a6180c77998809150101e4c51697401d3303
yimeantw <yimeantw@yahoo.com.tw>
2018-05-23 23:10:51 +0800

    Add files via upload
    This is a practice

---
+++ print_information_to_computer.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);  
+
+}
+byte val=65;
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0;i<26;i++){
+    Serial.write(val+i);
+    Serial.print('=');
+    Serial.println(val+i);
+    delay(1000);
+  }
+}
